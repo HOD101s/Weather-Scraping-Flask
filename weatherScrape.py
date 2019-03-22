85887c83b8963f2d382f9db84b0c247c0e60389f
Sanket Dalvi <36796116+sank8dalvi@users.noreply.github.com>
2019-03-23 01:16:26 +0530

    Updated weatherscape.py

--- weatherScrape.py
+++ weatherScrape.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape(city):
-	city = city.replace(' ','-')
+	city=city.replace(' ','-')
 	URL="https://www.weather-forecast.com/locations/"+city
 	webpage = requests.get(URL)
-	if webpage.status_code==requests.codes.ok:
+	if webpage.status_code==200:
 		print("Connected")
-		soup = BeautifulSoup(webpage.content, "html.parser")
-		wdata = soup.find('span',class_="phrase").text
-		wdata = '<div class="alert alert-success" role="alert">'+wdata+'</div>'
-		return wdata
+		soup = BeautifulSoup(webpage.content,"html.parser")
+		wdata=soup.find('span',class_="phrase").text
+		wdata='<div class="alert alert-success" role="alert">'+wdata+'</div>'
 	else:
 		print("Failed to Connect")
-		return '<div class="alert alert-danger" role="alert">The entered city is not valid.</div>'
+		wdata='<div class="alert alert-danger" role="alert">The entered city is not valid</div>'
+	return wdata
